13d444e5d254d279edfed33efd4ca6c9fd55d7a5
helox <eyes22798@gmail.com>
2023-09-28 16:16:03 +0800

    chore: github pages

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { PluginFunction } from "vue"
-import TreeOrg from './tree.vue'
-export const CustomTreeOrg  = TreeOrg
-
-const install: PluginFunction<null> = function (Vue) {
-  Vue.component(TreeOrg.name, CustomTreeOrg)
-}
-
-export default install
